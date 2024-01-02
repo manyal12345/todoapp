2aff2d64499de71117f7a476e4977f103dcf646b
Pramod Singh Manyal <manyalp12345@gmail.com>
2024-01-02 07:39:42 +0545

    Todo App final

--- base/views.py
+++ base/views.py
@@ -1,14 +1,42 @@
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.shortcuts import render
-from base.models import Todo
+from .models import Todo
 
 # Create your views here.
-
 def home(request):
     todos = Todo.objects.all()
-    context = {'todos': todos}
-    print(todos)
-    return render(request, 'index.html', context)
+    content = {'todos':todos}
+    return render(request,'index.html',context=content)
 
 def create(request):
-    return render(request, 'create.html')
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        status = request.POST.get('status')
+        Todo.objects.create(name=name,description=description,status=status)
+        return redirect('home')
+    content = {'mode':'create'}
+    return render(request,'create.html',context=content)
+
+def edit(request,pk):
+    todo = Todo.objects.get(id=pk)
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        status = request.POST.get('status')
+        todo.name = name
+        todo.description = description
+        todo.status = status
+        todo.save()
+        return redirect('home')
+    content = {'mode':'edit','todo':todo}
+    return render(request,'create.html',context=content)
+
+def delete(request,pk):
+    todo = Todo.objects.get(id=pk)
+    todo.delete()
+    return redirect('home')
+
+def delete_all(request):
+    Todo.objects.all().delete()
+    return redirect('home')
